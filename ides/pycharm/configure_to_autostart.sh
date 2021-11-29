3233a4d7021fa0f824aa8caf2e637d3c4d8d04dd
Jan Van Bruggen <JVanBruggen@itopia.com>
2021-11-29 19:24:31 +0000

    Add sudo to /etc/skel/ commands

--- ides/pycharm/configure_to_autostart.sh
+++ ides/pycharm/configure_to_autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp jetbrains-pycharm-ce.desktop /etc/skel/Autostart/
+sudo cp jetbrains-pycharm-ce.desktop /etc/skel/Autostart/
--- ides/pycharm/create_desktop_shortcut.sh
+++ ides/pycharm/create_desktop_shortcut.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp jetbrains-pycharm-ce.desktop /etc/skel/Desktop/
+sudo cp jetbrains-pycharm-ce.desktop /etc/skel/Desktop/
