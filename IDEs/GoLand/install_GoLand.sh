6f054e598314786626b674ce90aa1cd77aafef0e
Jan Van Bruggen <JVanBruggen@itopia.com>
2021-12-22 08:35:08 +0000

    Make /opt/ directory names more consistent

--- IDEs/GoLand/install_GoLand.sh
+++ IDEs/GoLand/install_GoLand.sh
@@ -7,12 +7,12 @@ curl -LOs https://download.jetbrains.com/go/goland-2021.3.tar.gz
 curl -LOs https://download.jetbrains.com/go/goland-2021.3.tar.gz.sha256
 sha256sum -c goland-2021.3.tar.gz.sha256
 
-sudo mkdir /opt/goland
+sudo mkdir /opt/GoLand
 sudo tar xzf goland-2021.3.tar.gz \
-    -C /opt/goland \
+    -C /opt/GoLand \
     --strip-components 1
 
-sudo ln -sf /opt/goland/bin/goland.sh /usr/local/bin/goland
+sudo ln -sf /opt/GoLand/bin/goland.sh /usr/local/bin/goland
 
 cd /
 rm -rf /tmp/install-goland
