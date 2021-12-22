b8eb92c4da850d44d4cf3509e02149280c7a113f
Jan Van Bruggen <JVanBruggen@itopia.com>
2021-12-22 07:18:41 +0000

    Remove automation echoes, as they'll age poorly

--- runtimes/OpenJDK/install_OpenJDK_8_on_Ubuntu_focal.sh
+++ runtimes/OpenJDK/install_OpenJDK_8_on_Ubuntu_focal.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# Note: This echo command exists for image update automation.
-#       When a new patch version is released,
-#       update this echo command to invalidate the layer cache.
-echo "Installing OpenJDK 1.8.0_292-8u292-b10-0ubuntu1~20.04-b10"
-
 sudo apt-get -y install openjdk-8-jdk
