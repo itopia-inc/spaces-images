e7f3961223fd9ddbc9812054f2257d0831a557a4
Dan Isla <disla@itopia.com>
2022-01-26 06:47:44 +0000

    [PyCharm IDE] update autostart to support opening fullscreen with repo

--- IDEs/PyCharm/create_PyCharm_desktop_shortcut.sh
+++ IDEs/PyCharm/create_PyCharm_desktop_shortcut.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo cp /usr/share/dev-scripts/PyCharm_CE.desktop /etc/skel/Desktop/
+sudo sed -i 's,/usr/share/dev-scripts/start_pycharm_with_repo.sh,env VDI_launchIDE=pycharm /usr/share/dev-scripts/start_pycharm_with_repo.sh,g' /etc/skel/Desktop/PyCharm_CE.desktop
