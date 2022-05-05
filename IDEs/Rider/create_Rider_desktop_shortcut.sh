ece4fd1efbddf784b6a2b44d42f9d7891ab72f60
Jan Van Bruggen <JVanBruggen@itopia.com>
2022-05-05 20:32:53 +0000

    Fix rebase typos from #33 & #36

--- IDEs/Rider/create_Rider_desktop_shortcut.sh
+++ IDEs/Rider/create_Rider_desktop_shortcut.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo cp /usr/share/dev-scripts/Rider.desktop /etc/skel/Desktop/
-sudo sed -i 's,/usr/share/dev-scripts/start_rider_with_repo.sh,env VDI_launchIDE=goland /usr/share/dev-scripts/start_goland_with_repo.sh,g' /etc/skel/Desktop/Rider.desktop
+sudo sed -i 's,/usr/share/dev-scripts/start_Rider_with_repo.sh,env VDI_launchIDE=goland /usr/share/dev-scripts/start_goland_with_repo.sh,g' /etc/skel/Desktop/Rider.desktop
